bed687e38f171fba19eb4abf9baef1514686f7e2
triplek <triplek@triplek.net>
2022-10-06 17:21:13 +0900

    #5.2 Custom Fields

--- users/models.py
+++ users/models.py
@@ -1,5 +1,12 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    email = models.EmailField(
+        verbose_name="email address", unique=True, blank=True)
+    name = models.CharField(max_length=150)
+    is_host = models.BooleanField()
