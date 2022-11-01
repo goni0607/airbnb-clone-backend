eb829b6788e4361e8daec4add7877ebff6837439
triplek <triplek@triplek.net>
2022-11-01 17:36:35 +0900

    #10.3 Serializer

--- categories/views.py
+++ categories/views.py
@@ -3,13 +3,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
 
 @api_view()
 def categories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
     return Response(
         {
             "ok": True,
+            "categories": serializer.data,
         }
     )
     # categories = Category.objects.all()
