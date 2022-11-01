b53082531ca1b88f41e09fe8fb18d811e4b8b07a
triplek <triplek@triplek.net>
2022-11-01 18:19:40 +0900

    #10.4 POST Requests

--- categories/views.py
+++ categories/views.py
@@ -6,16 +6,23 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(
-        {
-            "ok": True,
-            "categories": serializer.data,
-        }
-    )
+
+    if request.method == "GET":
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
     # categories = Category.objects.all()
     # json_data = serializers.serialize("json", categories)
     # return JsonResponse({'ok': True, 'categories': json_data, })
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
