6fc8fa5d150fbdacab35ec846966026663849651
triplek <triplek@triplek.net>
2022-11-03 09:05:20 +0900

    #10.7 updte(), #10.8 DELETE

--- categories/views.py
+++ categories/views.py
@@ -2,6 +2,8 @@
 # from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -26,8 +28,24 @@ def categories(request):
     # return JsonResponse({'ok': True, 'categories': json_data, })
 
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise HTTP_404_NOT_FOUND
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
