b88a647da83c6f10802c9b302986408784d15d30
triplek <triplek@triplek.net>
2022-11-03 10:42:59 +0900

    #10.12 ModelViewSet

--- categories/views.py
+++ categories/views.py
@@ -5,47 +5,53 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class Categories(APIView):
+class CategoryViewSet(ModelViewSet):
 
-    def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
 
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(CategorySerializer(new_category).data)
-        else:
-            return Response(serializer.errors)
+# class Categories(APIView):
+
+#     def get(self, request):
+#         categories = Category.objects.all()
+#         serializer = CategorySerializer(categories, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CategorySerializer(data=request.data)
+#         if serializer.is_valid():
+#             new_category = serializer.save()
+#             return Response(CategorySerializer(new_category).data)
+#         else:
+#             return Response(serializer.errors)
 
 
-class CategoryDetail(APIView):
+# class CategoryDetail(APIView):
 
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise HTTP_404_NOT_FOUND
+#     def get_object(self, pk):
+#         try:
+#             return Category.objects.get(pk=pk)
+#         except Category.DoesNotExist:
+#             raise HTTP_404_NOT_FOUND
 
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(serializer.data)
+#     def get(self, request, pk):
+#         serializer = CategorySerializer(self.get_object(pk))
+#         return Response(serializer.data)
 
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk), data=request.data, partial=True)
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
+#     def put(self, request, pk):
+#         serializer = CategorySerializer(
+#             self.get_object(pk), data=request.data, partial=True)
+#         if serializer.is_valid():
+#             updated_category = serializer.save()
+#             return Response(CategorySerializer(updated_category).data)
+#         else:
+#             return Response(serializer.errors)
 
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         self.get_object(pk).delete()
+#         return Response(status=HTTP_204_NO_CONTENT)
