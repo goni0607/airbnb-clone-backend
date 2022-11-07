84766c726d7cd7033b8e7687fa081f0bd8635729
triplek <triplek@triplek.net>
2022-11-07 15:20:47 +0900

    #11.23 Create a booking

--- rooms/views.py
+++ rooms/views.py
@@ -4,15 +4,17 @@ from django.utils import timezone
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError, PermissionDenied
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_200_OK
 from rest_framework.views import APIView
 from .models import Amenity, Room
 from .serializers import AmenitySerializer, RoomDetailSerializer, RoomListSerializer, RoomDetailSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 from categories.models import Category
 from medias.serializers import PhotoSerializer
 from reviews.serializers import ReviewSerializer
+
+from rooms import serializers
 
 
 class Rooms(APIView):
@@ -284,3 +286,11 @@ class RoomBookings(APIView):
         )
         serializer = PublicBookingSerializer(bookings, many=True)
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(status=HTTP_200_OK)
+        else:
+            return Response(serializer.errors)
